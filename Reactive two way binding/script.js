eca4c391d50a59ad81e040f054933ff15b6d94eb
Whitetail11 <43177664+Whitetail11@users.noreply.github.com>
2020-03-28 15:15:00 +0200

    form

---
+++ Reactive two way binding/script.js
@@ -0,0 +1,18 @@
+const myBind = function(idt, obj, field) {
+    
+}
+
+const pricing = {
+    "MGM Grand": 1000,
+    "CityCenter": 1500,
+    "The Palazzo": 700,
+    "The Mirage": 300
+}
+
+let Hotel = {
+    startdate: "yyy-mm-dd",
+    enddate: "yyy-mm-dd",
+    name: "",
+    sum: 0,
+    days: 0
+}
