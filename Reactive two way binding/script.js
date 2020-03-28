93d1dc752ec8154c6eff1f374914224c7dfdb0ad
Whitetail11 <43177664+Whitetail11@users.noreply.github.com>
2020-03-28 18:37:57 +0200

    two way binding

--- Reactive two way binding/script.js
+++ Reactive two way binding/script.js
@@ -1,6 +1,32 @@
-const myBind = function(idt, obj, field) {
-    
+function MyBind(idt, obj, field) {
+    let input = document.getElementById(idt);
+    input.addEventListener('change', function(e) {
+        obj[field] = e.target.value;
+        calc();
+    });
+    Object.defineProperty(obj, field, {
+        get: function() {return this['__' + field];},
+        set: function(value) {
+            if(this[field] === value)
+            return;
+            this['__' + field] = value;
+            input.value = value;
+        }
+    })
 }
+
+function MyBindRadio(name, obj, field) {
+    let inputs = document.getElementsByName(name);
+    inputs.forEach((input) => {
+        input.addEventListener('change', function(e) {
+            obj[field] = e.target.value;
+            calc();
+        });
+    });
+    
+   
+}
+
 
 const pricing = {
     "MGM Grand": 1000,
@@ -12,7 +38,27 @@ const pricing = {
 let Hotel = {
     startdate: "yyy-mm-dd",
     enddate: "yyy-mm-dd",
-    name: "",
+    name: "MGM Grand",
     sum: 0,
-    days: 0
 }
+
+
+function calc() {
+    if(Hotel.startdate && Hotel.enddate && Hotel.name) {
+      const days = document.getElementById("days");
+      days.value = Math.round((new Date(Hotel.enddate) - new Date(Hotel.startdate))/(24*60*60*1000));
+      if(pricing[Hotel.name]) {
+        Hotel.sum = pricing[Hotel.name] * days.value;
+      }
+    }
+  }
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    MyBind("startdate", Hotel, 'startdate');
+    MyBind("enddate", Hotel, 'enddate');
+    MyBind("sum", Hotel, 'sum');
+    MyBindRadio("hotel", Hotel, 'name');
+});
+
+    
