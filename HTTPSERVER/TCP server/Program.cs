f9af6cb9ce06dbcaef012319db02c2ca7edde4ef
Whitetail11 <43177664+Whitetail11@users.noreply.github.com>
2020-04-19 19:40:44 +0300

    tcpwebserver

--- HTTPSERVER/TCP server/Program.cs
+++ HTTPSERVER/TCP server/Program.cs
@@ -12,27 +12,27 @@ namespace TCP_server
             Console.WriteLine("Simple server");
             try
             {
-                HttpListener listener = new HttpListener();
-                listener.Prefixes.Add("http://127.0.0.1:13204/");
-                listener.Start();
-                Console.WriteLine("Ожидание подключений");
-                while(true)
-                {
-                    HttpListenerContext context = listener.GetContext();
-                    HttpListenerRequest request = context.Request;
-                    HttpListenerResponse response = context.Response;
-                    string responseM = "<html><head><meta charset='utf8'></head><body>Hello webserver</body></html>";
-                    byte[] buffer = System.Text.Encoding.ASCII.GetBytes(responseM);
-                    response.ContentLength64 = buffer.Length;
-                    Stream output = response.OutputStream;
-                    output.Write(buffer,0,buffer.Length);
-                    output.Close();
-                    listener.Stop();
-                    Console.WriteLine("Обработка подключений завершена");
-                    Console.Read();
-                }
-               /* var port = 19539;
-                var localAddr = IPAddress.Parse("127.0.0.1");
+                //HttpListener listener = new HttpListener();
+                //listener.Prefixes.Add("http://127.0.0.1:13204/");
+                //listener.Start();
+                //Console.WriteLine("Ожидание подключений");
+                //while (true)
+                //{
+                //    HttpListenerContext context = listener.GetContext();
+                //    HttpListenerRequest request = context.Request;
+                //    HttpListenerResponse response = context.Response;
+                //    string responseM = "<html><head><meta charset='utf8'></head><body>Hello webserver</body></html>";
+                //    byte[] buffer = System.Text.Encoding.ASCII.GetBytes(responseM);
+                //    response.ContentLength64 = buffer.Length;
+                //    Stream output = response.OutputStream;
+                //    output.Write(buffer, 0, buffer.Length);
+                //    output.Close();
+                //    listener.Stop();
+                //    Console.WriteLine("Обработка подключений завершена");
+                //    Console.Read();
+                //}
+                var port = 12000;
+                var localAddr = IPAddress.Any;
                 var server = new TcpListener(localAddr, port);
                 server.Start();
                 var bytes = new Byte[256];
@@ -42,16 +42,27 @@ namespace TCP_server
                     var client = server.AcceptTcpClient();
                     Console.WriteLine("Accepted connection from {0}", client.Client.RemoteEndPoint);
                     NetworkStream stream = client.GetStream();
+                    StreamWriter writer = new StreamWriter(stream);
                     var bytesReadCount = stream.Read(bytes, 0, bytes.Length);
                     var data = System.Text.Encoding.ASCII.GetString(bytes, 0, bytesReadCount);
-                    data = data.ToUpper();
-                    var response = System.Text.Encoding.ASCII.GetBytes("your massage is taken");
-                    stream.Write(response, 0, response.Length);
-                    Console.WriteLine("Sent : your massage is taken");
-                    client.Close();
+                   
+                    Byte[] msg = System.Text.Encoding.ASCII.GetBytes("<html><head><meta charset='utf8'></head><body>Hello webserver</body></html>");
 
-                }*/
-            }
+                    var res = System.Text.Encoding.ASCII.GetBytes("HTTP/1.1 200 OK\r\nServer: danyaserv/1.1\r\nContent-Language: ru\r\nContent-Type: text/html\r\nAccept-Ranges: bytes\r\nContent-Lenght: 75\r\nConnection: close\r\nAccept-Encoding: gzip, deflate\r\n\r\n");
+                    stream.Write(res, 0, res.Length);
+                    //writer.WriteLine(String.Format("HTTP/1.1 200 OK\r\nServer: danyaserv/1.1\r\nContent-Language: ru\r\nContent-Type: text/html\r\nAccept-Ranges: bytes\r\nContent-Lenght: {0}\r\nConnection: close\r\nAccept-Encoding: gzip, deflate\r\n", msg.Length));
+                   // writer.Flush();
+                    stream.Write(msg, 0, msg.Length);
+                    Console.WriteLine("Recived: {0}", data);
+                    
+                    //Console.WriteLine("Sent : {0}", ms);
+                    Console.WriteLine("sent: HTTP/1.1 200 OK\r\nServer: danyaserv/1.1\r\nContent-language: ru\r\nContent-type: text/html\r\nAccept-Ranges: bytes\r\nContent-Lenght: {0}\r\nConnection: close\r\n", msg.Length);
+                    writer.Close();
+                    stream.Close();
+                    
+                }
+            }                
+                    
             catch(Exception e)
             {
                 Console.WriteLine("Exception {0}", e);
